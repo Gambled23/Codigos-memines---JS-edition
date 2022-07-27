ec93fe737e1263370bf78fa60720b99fa1dc6249
Cesar <ipog71@gmail.com>
2022-07-27 15:28:24 -0500

    prueba de funcion de flechas

---
+++ Pruebas/templateStrings.js
@@ -0,0 +1,8 @@
+const suma = (num1, num2) => {
+    return `El resultado de la suma es ${num1 + num2}` 
+}
+
+const sumaDiez = variable1 => variable1 + 10
+
+console.log(suma(15,10))
+console.log(sumaDiez(3))
