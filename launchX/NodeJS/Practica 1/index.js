2e19245a5a3e4f6d7abb4a44a4d0bb7fa6ae3d2d
Cesar <ipog71@gmail.com>
2022-12-02 16:03:10 -0600

    2do archivo

--- launchX/NodeJS/Practica 1/index.js
+++ launchX/NodeJS/Practica 1/index.js
@@ -1,13 +1,10 @@
-let http = require('http'); //Obtener http
-const rd = require('./modules/retrievedate');
-const url = require('url'); //Obtener URL
-//console.log(http);
+const http = require('http');
+const fs = require('fs');
 
-http.createServer(function (req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('La fecha y hora es: ' + rd.RetrieveDate());
-    const query = url.parse(req.url, true).query;
-    const text = query.year + ' ' + query.month;
-
-    res.end('Hola mundo'); //El end es neceario para decir cuando acaba
-}).listen(8080)
+http.createServer(function(req, res){
+    fs.readFile('./html/demo.html', function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data) //La data es el archivo html
+        return res.end();
+    });
+}).listen(8080);
