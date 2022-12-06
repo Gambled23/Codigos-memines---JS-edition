3a6cd00abe36789fa0d8521117fa476eb80a1140
Cesar <ipog71@gmail.com>
2022-12-06 16:18:47 -0600

    Registrar usuario con POST

---
+++ launchX/mostrar pagina nodejs/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+//Como el post manda un json, debemos decirle a JS que va a leer json
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//Hacer que node nos rediriga entre paginas de la navbar
+router.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname + '/templates/index.html'))
+})
+router.get('/contacto', (req, res) =>{
+    res.sendFile(path.join(__dirname + '/templates/contacto.html'))
+})
+router.get('/perfil', (req, res) =>{
+    res.sendFile(path.join(__dirname + '/templates/perfil.html'))
+})
+
+router.post('/', (req, res) =>{
+    // console.log(req.body); req.body es lo que se manda
+    res.send(`El usuario ${req.body.first_name} ha sido registrado`)
+})
+
+app.use('/', router);
+app.listen(process.env.port || 8080); //Process es una variable de entorno
+
+console.log('Activo en el puerto 8080')
